442e8fd12bd8fb18b46c78223c1de41615738c56
Gary Newby <gary@mellowmuse.com>
2021-03-30 15:09:05 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MUXSDK",
-            url: "https://github.com/garynewby/MUXSDK/raw/main/Sources/MUXSDK/MUX.zip",
+            url: "https://github.com/garynewby/MUXSDK/blob/main/Sources/MUXSDK/MUX.zip?raw=true",
             checksum: "2e7cb5ec68106f5f8de7e509d94830bcf18a98592cd3217b74ccb08f2c7d3a5c"
         ),
     ]
